21ca115a6ebdc5177601c671cace286f8187a0e2
avj-coder <aleksej.afonin@gmail.com>
2023-04-08 04:10:44 +0700

    lesson14 homework task1

---
+++ src/main/java/org/example/lesson14homework/Task1.java
@@ -0,0 +1,28 @@
+package org.example.lesson14homework;
+
+/**
+ * Задача 1
+ * Проверить, является ли введённая строка палиндромом,
+ * т.е. читается одинаково в обоих направлениях.
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        String line = "arozaupalanalapuazora";
+        boolean isPalindrom = isPalindrome(line);
+        System.out.println("String is palindrom: " + isPalindrom);
+    }
+
+    private static boolean isPalindrome(String line) {
+        int i = 0;
+        int j = line.length()-1;
+        while (i<j) {
+            if (line.charAt(i) != line.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
