4b0b11e7ca5e750825dee05d3449fe6a5ad19435
avj-coder <aleksej.afonin@gmail.com>
2023-04-08 01:12:00 +0700

    String practice lesson 13

--- src/main/java/org/example/lesson7/Computer.java
+++ src/main/java/org/example/lesson7/Computer.java
@@ -4,14 +4,24 @@ public class Computer {
 
     private Ram ram;
     private Ssd ssd;
+    private static int counter  = 0;
 
-    Computer(Ram ram, Ssd ssd) {
+    public Computer() {
+        counter++;
+    }
+
+    public Computer(Ram ram, Ssd ssd) {
         this.ram = ram;
         this.ssd = ssd;
+        counter++;
     }
 
     public void printState() {
         System.out.println("Computer: ram " + ram.getValue());
         System.out.println("Computer: ssd " + ssd.getValue());
+    }
+
+    public static int getCounter() {
+        return counter;
     }
 }
