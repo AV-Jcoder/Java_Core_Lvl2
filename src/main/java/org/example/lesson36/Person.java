cb7d33d5f54ecf7f980d657c5314a916a9a82055
avj-coder <aleksej.afonin@gmail.com>
2023-04-11 21:06:58 +0700

    lesson36 equals and hashCode

---
+++ src/main/java/org/example/lesson36/Person.java
@@ -0,0 +1,44 @@
+package org.example.lesson36;
+
+public class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        if (id != person.id) return false;
+        if (!firstName.equals(person.firstName)) return false;
+        return lastName.equals(person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
+    }
+}
