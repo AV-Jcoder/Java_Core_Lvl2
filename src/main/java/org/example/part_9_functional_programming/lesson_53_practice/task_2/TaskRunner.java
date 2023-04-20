24f30e610c90588bc69d6963c788723d4472b5f5
avj-coder <aleksej.afonin@gmail.com>
2023-04-21 06:59:45 +0700

    lesson53 task3

--- src/main/java/org/example/part_9_functional_programming/lesson_53_practice/task_2/TaskRunner.java
+++ src/main/java/org/example/part_9_functional_programming/lesson_53_practice/task_2/TaskRunner.java
@@ -1,9 +1,59 @@
 package org.example.part_9_functional_programming.lesson_53_practice.task_2;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * 2. Дан список строк. Найти количество уникальных
  *  * строк длиной более 8 символов.
  *  *
  */
 public class TaskRunner {
+
+    public static void main(String[] args) {
+        List<String> strings = List.of(
+                "string-1",
+                "string-2",
+                "string-3",
+                "string-4",
+                "string-10",
+                "string-10",
+                "string-10",
+                "string-12",
+                "string-16"
+        );
+
+        long count = strings.stream()
+                .filter(str -> str.length() > 8)
+                .distinct()
+                .count();
+        System.out.println(count);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        long count2 = strings.stream()
+                .filter(str -> str.length() > 8)
+                .collect(Collectors.toSet())
+                .size();
+        System.out.println(count2);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        printStrings(strings);
+    }
+
+    private static void printStrings(List<String> strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String string : strings) {
+            map.put(string, map.getOrDefault(string, 0)+1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getKey().length() > 8) {
+                System.out.println(entry.getKey()+ " : " + entry.getValue());
+            }
+        }
+    }
 }
--- src/main/java/org/example/part_9_functional_programming/lesson_53_practice/task_3/TaskRunner.java
+++ src/main/java/org/example/part_9_functional_programming/lesson_53_practice/task_3/TaskRunner.java
@@ -1,5 +1,7 @@
 package org.example.part_9_functional_programming.lesson_53_practice.task_3;
 
+import java.util.Map;
+
 /**
  *  *
  *  * 3. Дана Map<String, Integer>. Найти сумму всех
@@ -7,4 +9,33 @@ package org.example.part_9_functional_programming.lesson_53_practice.task_3;
  *  * символов.
  */
 public class TaskRunner {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = Map.of(
+                "string1", 1,
+                "strin2", 2,
+                "string3", 3,
+                "string4", 5,
+                "strin5", 5
+        );
+
+        int sum = map.entrySet().stream()
+                .filter(n -> n.getKey().length() < 7)
+                .mapToInt(Map.Entry::getValue)
+                .sum();
+
+        System.out.println(sum);
+
+//        printSum(map);
+    }
+
+    private static void printSum(Map<String, Integer> map) {
+        int result = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getKey().length() < 7) {
+                result += entry.getValue();
+            }
+        }
+        System.out.println(result);
+    }
 }
