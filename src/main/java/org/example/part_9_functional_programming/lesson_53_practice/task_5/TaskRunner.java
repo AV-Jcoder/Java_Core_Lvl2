fc55846b42b695d6c6c2e70da933c0d22d773b22
avj-coder <aleksej.afonin@gmail.com>
2023-04-21 08:02:13 +0700

    lesson53 task5

--- src/main/java/org/example/part_9_functional_programming/lesson_53_practice/task_5/TaskRunner.java
+++ src/main/java/org/example/part_9_functional_programming/lesson_53_practice/task_5/TaskRunner.java
@@ -1,5 +1,8 @@
 package org.example.part_9_functional_programming.lesson_53_practice.task_5;
 
+import java.util.Comparator;
+import java.util.List;
+
 /**
  *  * 5. Дан класс Person с полями firstName, lastName,
  *  * age.
@@ -8,4 +11,21 @@ package org.example.part_9_functional_programming.lesson_53_practice.task_5;
  *  * символов.
  */
 public class TaskRunner {
+
+    public static void main(String[] args) {
+        List<Person> persons = List.of(
+                new Person("Ivan", "Ivanov", 20),
+                new Person("Petr", "Petrov", 25),
+                new Person("Sveta", "Svetikova", 33),
+                new Person("Kate", "Ivanova", 45),
+                new Person("Slava", "Slavikov", 18),
+                new Person("Arni", "Kutuzov12324", 56)
+        );
+
+        persons.stream()
+                .filter(pers -> pers.getFullName().length() < 15)
+                .max(Comparator.comparing(Person::getAge))
+                .ifPresent(System.out::println);
+
+    }
 }
