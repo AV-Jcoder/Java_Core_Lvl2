d20274d12963965167879a092785b945af7c9dbf
avj-coder <aleksej.afonin@gmail.com>
2023-04-21 07:35:05 +0700

    lesson53 task4

--- src/main/java/org/example/part_9_functional_programming/lesson_53_practice/task_4/TaskRunner.java
+++ src/main/java/org/example/part_9_functional_programming/lesson_53_practice/task_4/TaskRunner.java
@@ -1,5 +1,8 @@
 package org.example.part_9_functional_programming.lesson_53_practice.task_4;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 4. Дан список целых чисел, вывести строку,
  * представляющую собой конкатенацию
@@ -9,4 +12,19 @@ package org.example.part_9_functional_programming.lesson_53_practice.task_4;
  *
  */
 public class TaskRunner {
+
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(5, 2, 4, 2, 1);
+        integers.stream()
+                .map(String::valueOf)
+                .reduce(String::concat)
+                .ifPresent(System.out::println);
+
+
+        String concat = integers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        System.out.println(concat);
+
+    }
 }
