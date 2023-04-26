afb978d707ed2f0c89a2c66baf067aa7f4a24d7b
avj-coder <aleksej.afonin@gmail.com>
2023-04-26 18:41:48 +0700

    lesson55

---
+++ src/main/java/org/example/part_10_Input_Output_Stream/lesson_55/InputStreamRunner.java
@@ -0,0 +1,25 @@
+package org.example.part_10_Input_Output_Stream.lesson_55;
+
+import java.io.*;
+import java.nio.file.Path;
+
+public class InputStreamRunner {
+
+    public static void main(String[] args) {
+//        File file = new File(String.join(File.separator, "resources", "test.txt"));
+        File file = Path.of("resources", "test.txt").toFile();
+        try (FileInputStream is = new FileInputStream(file)) {
+            byte[] buff = new byte[8];
+            int countBytes = 0;
+            while((countBytes = is.read(buff)) > 0) {
+                String text = new String(buff, 0, countBytes);
+                System.out.print(text);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("IO exception");
+            e.printStackTrace();
+        }
+    }
+}
