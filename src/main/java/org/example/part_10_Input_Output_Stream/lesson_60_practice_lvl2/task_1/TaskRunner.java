4da68f278c90e76fa12d56ee83784ca8cd98d80b
avj-coder <aleksej.afonin@gmail.com>
2023-04-30 15:39:38 +0700

    lesson60 lvl2 task1

--- src/main/java/org/example/part_10_Input_Output_Stream/lesson_60_practice_lvl2/task_1/TaskRunner.java
+++ src/main/java/org/example/part_10_Input_Output_Stream/lesson_60_practice_lvl2/task_1/TaskRunner.java
@@ -1,5 +1,7 @@
 package org.example.part_10_Input_Output_Stream.lesson_60_practice_lvl2.task_1;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,13 +21,16 @@ import java.util.Map;
 public class TaskRunner {
 
     public static void main(String[] args) throws IOException {
-        Path path = Path.of("resources", "text.txt");
-        String text = Files.readString(path);
-        Map<Character, Integer> map = getCharactersCount(text);
+        Path srcPath = Path.of("resources", "text.txt");
+        String text = Files.readString(srcPath);
+        Map<Character, Integer> map = groupCharactersByFrequency(text);
         printCharacters(map);
+
+        Path destPath = Path.of("resources", "grouped.txt");
+        writeGroupedCharacters(map, destPath);
     }
 
-    private static Map<Character, Integer> getCharactersCount(String text) {
+    private static Map<Character, Integer> groupCharactersByFrequency(String text) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < text.length(); i++) {
             Character ch = Character.toLowerCase(text.charAt(i));
@@ -37,6 +42,22 @@ public class TaskRunner {
     private static void printCharacters(Map<Character, Integer> map) {
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
+    private static void writeGroupedCharacters(Map<Character, Integer> map, Path destPath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(destPath.toFile()))) {
+            if (Files.notExists(destPath)) {
+                Files.createFile(destPath);
+            }
+            StringBuilder line = new StringBuilder();
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                line.append(entry.getKey()).append(" : ").append(entry.getValue());
+                line.append(System.lineSeparator());
+            }
+            writer.write(line.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
