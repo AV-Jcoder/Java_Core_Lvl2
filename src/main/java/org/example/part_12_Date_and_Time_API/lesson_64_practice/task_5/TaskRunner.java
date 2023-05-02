abe1f3a46c87bc7743168e673860973b2b7fa951
avj-coder <aleksej.afonin@gmail.com>
2023-05-03 01:17:58 +0700

    lesson64 task5

--- src/main/java/org/example/part_12_Date_and_Time_API/lesson_64_practice/task_5/TaskRunner.java
+++ src/main/java/org/example/part_12_Date_and_Time_API/lesson_64_practice/task_5/TaskRunner.java
@@ -1,6 +1,9 @@
 package org.example.part_12_Date_and_Time_API.lesson_64_practice.task_5;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
  * 5.
@@ -15,6 +18,12 @@ public class TaskRunner {
         LocalDate date1 = LocalDate.now();
         LocalDate date2 = LocalDate.of(2018, 7, 7);
 
+
+        LocalDateTime ldt1 = date1.atStartOfDay();
+        LocalDateTime ldt2 = LocalDateTime.of(date2, LocalTime.MIDNIGHT);
+
+        Duration duration = Duration.between(ldt2, ldt1);
+        System.out.println("Duratioin between dates: " + duration.getSeconds());
 
     }
 }
