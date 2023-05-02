24605dc4b8dba1476eaa3441229ba54b58363953
avj-coder <aleksej.afonin@gmail.com>
2023-05-03 01:53:46 +0700

    lesson64 task7

--- src/main/java/org/example/part_12_Date_and_Time_API/lesson_64_practice/task_7/TaskRunner.java
+++ src/main/java/org/example/part_12_Date_and_Time_API/lesson_64_practice/task_7/TaskRunner.java
@@ -1,5 +1,8 @@
 package org.example.part_12_Date_and_Time_API.lesson_64_practice.task_7;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 /**
  * 7. Написать свою реализацию интерфейса TemporalAdjuster, которая бы
  * прибавляла к дате 42 дня
@@ -7,6 +10,11 @@ package org.example.part_12_Date_and_Time_API.lesson_64_practice.task_7;
 public class TaskRunner {
 
     public static void main(String[] args) {
-        
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+
+        LocalDateTime adjustDateTime = ldt.with(temporal -> temporal.plus(42L, ChronoUnit.DAYS));
+        System.out.println(adjustDateTime);
+
     }
 }
