ad5f603d952e7c338b944779875daadf047de082
avj-coder <aleksej.afonin@gmail.com>
2023-05-04 17:36:39 +0700

    lesson65

---
+++ src/main/java/org/example/part_13_Multi_Threading/lesson_65/SimpleThread.java
@@ -0,0 +1,14 @@
+package org.example.part_13_Multi_Threading.lesson_65;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " start:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i=" + i + ". Hello form concurrent thread: " + this.getName());
+        }
+    }
+}
---
+++ src/main/java/org/example/part_13_Multi_Threading/lesson_65/ThreadDemo.java
@@ -0,0 +1,21 @@
+package org.example.part_13_Multi_Threading.lesson_65;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + " start:");
+        SimpleThread simpleThread = new SimpleThread();
+        simpleThread.start();
+
+        try {
+            System.out.println("main joined:");
+            simpleThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(Thread.currentThread().getName() + " finish.");
+    }
+}
