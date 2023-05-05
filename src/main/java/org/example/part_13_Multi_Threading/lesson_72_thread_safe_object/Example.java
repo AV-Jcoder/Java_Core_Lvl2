2f689eb642d73dac6583e57a7e50b5d5a3612f22
avj-coder <aleksej.afonin@gmail.com>
2023-05-05 19:05:55 +0700

    lesson 72

--- src/main/java/org/example/part_13_Multi_Threading/lesson_72_thread_safe_object/Example.java
+++ src/main/java/org/example/part_13_Multi_Threading/lesson_72_thread_safe_object/Example.java
@@ -1,2 +1,25 @@
-package org.example.part_13_Multi_Threading.lesson_72_thread_safe_object;public class Example {
+package org.example.part_13_Multi_Threading.lesson_72_thread_safe_object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Для создания потокобезопасных обьектов можно:
+ *
+ * 1. Объявить все методы с модификатором synchronized.
+ * 2. Вызывать методы в блоке synchronized.
+ * 3. Сделать все поля обекта immutable, те объявить их final.
+ * 4. В методах-геттерах возвращать копию значения.
+ */
+public class Example {
+
+    private final List<Integer> list;
+
+    public Example(List<Integer> list) {
+        this.list = List.copyOf(list); // immutable list
+    }
+
+    public List<Integer> getList() {
+        return new ArrayList<>(list);
+    }
 }
--- src/main/java/org/example/part_13_Multi_Threading/lesson_72_thread_safe_object/Starter.java
+++ src/main/java/org/example/part_13_Multi_Threading/lesson_72_thread_safe_object/Starter.java
@@ -1,2 +1,15 @@
-package org.example.part_13_Multi_Threading.lesson_72_thread_safe_object;public class Starter {
+package org.example.part_13_Multi_Threading.lesson_72_thread_safe_object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        Example example = new Example(new ArrayList<Integer>());
+        List<Integer> list = example.getList();
+        list.add(56);
+
+        System.out.println(example.getList());
+    }
 }
